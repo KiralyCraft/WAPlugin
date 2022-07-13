2f02ab306756c7c41bd2937762b7c6f8cf1327c0
kiralycraft <alexucu@gmail.com>
2022-07-13 22:44:24 +0300

    Implemented popup injector logic

--- popup.js
+++ popup.js
@@ -11,6 +11,49 @@ function handleContentResponse(responseAction, responseData)
 }
 
 /*
+ *	This method checks if the current tab responds to any messages sent from the plugin, since they are not loaded by a content-script.
+ */
+function probeInjectors()
+{
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {action: "action_popup_injectors_probe"}, function(theResponse) 
+		{
+			var lastError = chrome.runtime.lastError;
+			
+			//This may break in Chrome updates, keep an eye on it
+			if (lastError && lastError.message.indexOf("Receiving end does not exist") !== -1) 
+			{
+				console.log("Injectors are not loaded. Loading them now.");
+				buildInjectors();
+			} 
+			else if (theResponse.action == "action_popup_injectors_probe_reply")
+			{
+				console.log("Injectors are loaded, we're good");
+			}
+		});
+	});
+}
+function buildInjectors()
+{
+	var contentScripts = chrome.runtime.getManifest().lazyContentScripts;
+	for (var scriptIndex in contentScripts)
+	{
+		var scriptName = contentScripts[scriptIndex];
+		
+		//Ghetto workaround for deep copying this thing
+		(function (scriptNameDeep)
+		{
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) 
+			{
+				chrome.scripting.executeScript({
+					target: {tabId: tabs[0].id, allFrames: true},
+					files: [ scriptNameDeep ]
+				});
+			});
+		})(scriptName);
+	}
+}
+/*
  * Called when the plugin's UI is loaded and accessible. May be borked on IE8 :)
  */
 function buildPluginUI() 
@@ -20,7 +63,8 @@ function buildPluginUI()
 	 */
 	if (GREET_BACKGROUND_PAGE)
 	{
-		chrome.runtime.sendMessage({action: "action_popup_visible"}, function(theResponse) {
+		chrome.runtime.sendMessage({action: "action_popup_visible"}, function(theResponse) 
+		{
 			console.log(theResponse);
 		});
 	}
@@ -32,7 +76,8 @@ function buildPluginUI()
 	if (GREET_CONTENT_PAGE)
 	{
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {action: "action_popup_visible"}, function(theResponse) {
+			chrome.tabs.sendMessage(tabs[0].id, {action: "action_popup_visible"}, function(theResponse) 
+			{
 				for (var responseEntry in theResponse)
 				{
 					handleContentResponse(theResponse[responseEntry].action,theResponse[responseEntry].data);
@@ -46,5 +91,7 @@ function buildPluginUI()
  * Called when the document 
  */
 document.addEventListener("DOMContentLoaded", function(event) { 
+	probeInjectors();
+	//buildInjectors(); //Don't always force the building of injectors, because they may already be injected
 	buildPluginUI();
 });
