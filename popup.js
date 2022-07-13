9ca7a7ba6561be8eb4208a647990505c5f645a83
kiralycraft <alexucu@gmail.com>
2022-07-13 21:26:38 +0300

    Define popup UI building logic

--- popup.js
+++ popup.js
@@ -1,26 +1,50 @@
-function render() {
-    chrome.tabs.getSelected(null, function (selectedTab) {
-        $('#hello').html('The plugin is functioning normally...');
-        chrome.runtime.getBackgroundPage(function (backgroundPage) {
-            var message = backgroundPage.message;
+const GREET_BACKGROUND_PAGE = false;
+const GREET_CONTENT_PAGE = true;
 
-            if (! message) {
-                $('#debug').html('No message do display');
-                return;
-            }
-
-            $('#debug').html('Last logging message: ' + message);
-//          You can even post a message to a backend service
-//          $.post("https://www.scs.ubbcluj.ro/plugin/logging.php", message);
-        });
-    });
-
-    setTimeout(function () {
-       window.close();
-    }, 3000);
-
+/*
+ * Handles scenarios for various actions indicated by the content page or background script.
+ */
+function handleContentResponse(responseAction, responseData)
+{
+	console.log(responseAction);
+	console.log(responseData);
 }
 
-$(document).ready(function () {
-    render();
+/*
+ * Called when the plugin's UI is loaded and accessible. May be borked on IE8 :)
+ */
+function buildPluginUI() 
+{
+	/*
+	 * Send a message to the background page - Let it know the thing is open now.
+	 */
+	if (GREET_BACKGROUND_PAGE)
+	{
+		chrome.runtime.sendMessage({action: "action_popup_visible"}, function(theResponse) {
+			console.log(theResponse);
+		});
+	}
+	
+	/*
+	 *	Send a message to the content page. A JSONArray response is expected, with unique actions that are to be treated here.
+	 *	The content of the JSONArray entry is expected to be an "action" (string field) and a "data" which may be whatever the handler wants.
+	 */
+	if (GREET_CONTENT_PAGE)
+	{
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {action: "action_popup_visible"}, function(theResponse) {
+				for (var responseEntry in theResponse)
+				{
+					handleContentResponse(theResponse[responseEntry].action,theResponse[responseEntry].data);
+				}
+			});
+		});
+	}
+}
+
+/*
+ * Called when the document 
+ */
+document.addEventListener("DOMContentLoaded", function(event) { 
+	buildPluginUI();
 });
