0139c70f4d65352d0a5f07aabaddaa93df482958
kiralycraft <alexucu@gmail.com>
2022-08-25 12:58:38 +0300

    Add temporary button for table detection

--- popup.js
+++ popup.js
@@ -192,6 +192,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	//buildInjectors(); //Don't always force the building of injectors, because they may already be present
 });
 
+/*
+ * Asta merge la conceptDetect-SELECT_ALL, temporar
+ */
+document.querySelector('#processDOMDifferenceButtonTMP').addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {request: 'message_popup_page_processDOMDifference_TMP'});
+	});
+});
 
 document.querySelector('#processDOMDifferenceButton').addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
